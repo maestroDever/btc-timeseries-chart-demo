68b875324f234ce9f0d5fd5b38e049e36b26122b
chris <chrisboujian90@gmail.com>
2023-02-24 09:11:38 -0500

    feat: parse csv into json

--- pages/api/btc-addresses.ts
+++ pages/api/btc-addresses.ts
@@ -1,7 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+import Papa from "papaparse";
+import fs from "fs";
+import path from "path";
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
   // Return formatted coinmetrics data here
+  let btcData;
+  try {
+    const dataDirectory = path.join(process.cwd(), "data");
+    const fullPath = path.join(dataDirectory, "Coin_Metrics_Network_Data_2023-02-02T14-32.csv");
+    const fileContent = fs.readFileSync(fullPath, "utf16le");
 
-  res.status(200).json([])
-}
+
+    Papa.parse(fileContent, {
+      header: true,
+      delimiter: "\t",
+      complete: (res) => {
+        btcData = {
+          data: res.data,
+          headers: res.meta.fields
+        };
+      }
+    });
+  } catch (error) {
+    res.status(500).send('Error on parsing the request.')
+  } finally {
+    res.status(200).json(btcData);
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,30 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
-import {GetStaticProps} from 'next'
+import Head from "next/head";
+import Layout from "../components/layout";
+import { GetStaticProps } from "next";
 
-export default function Home() {
-    return (
-        <Layout home>
-            <Head>
-                <title>BTC Address Balances over Time</title>
-            </Head>
-            <section>
-                <div className="max-w-2xl mx-auto p-8 text-center">Chart goes here</div>
-            </section>
-        </Layout>
-    )
+import Papa from "papaparse";
+import fs from "fs";
+import path from "path";
+
+export default function Home(props: { data: any; headers: any }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>BTC Address Balances over Time</title>
+      </Head>
+      <section>
+        <div className="max-w-2xl mx-auto p-8 text-center">Chart goes here</div>
+      </section>
+    </Layout>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {}
-    }
-}
+  const res = await fetch("http://localhost:3000/api/btc-addresses");
+  const data = await res.json();
+
+  console.log({ data });
+  return {
+    props: {},
+  };
+};
